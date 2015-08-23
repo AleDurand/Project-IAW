7bff7115e0c7fec6bf69c8ac9a18ce55405a2f6f
matiasvarela <mtsvrl@gmail.com>
2015-08-23 20:06:54 -0300

    Updated CommerceRepository

--- Proyect/src/main/java/proyect/repositories/CommerceRepository.java
+++ Proyect/src/main/java/proyect/repositories/CommerceRepository.java
@@ -2,11 +2,11 @@ package proyect.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import proyect.model.Student;
+import proyect.model.Commerce;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long>{
+public interface CommerceRepository extends JpaRepository<Commerce, Long>{
 	
-	public Student findByLu(long lu);
+	public Commerce findById(long id);
 
 }
