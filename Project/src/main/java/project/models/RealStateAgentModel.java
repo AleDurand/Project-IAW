28d3b4af71ccc548174106ca87ebaac63ac48d01
Matias Varela <mtsvrl@gmail.com>
2015-08-29 18:49:10 -0300

    Completed UserModel and RealStateAgentModel

--- Project/src/main/java/project/models/RealStateAgentModel.java
+++ Project/src/main/java/project/models/RealStateAgentModel.java
@@ -1,5 +1,83 @@
 package project.models;
 
-public class RealStateAgentModel {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class RealStateAgentModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
+
+	@Column(name = "name", unique = true, nullable = false)
+	private String name;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "web")
+	private String web;
+
+	@Column(name = "email")
+	private String email;
+
+	public RealStateAgentModel() {
+
+	}
+
+	public RealStateAgentModel(Integer id, String name, String description, String web, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.web = web;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
--- Project/src/main/java/project/models/UserModel.java
+++ Project/src/main/java/project/models/UserModel.java
@@ -1,5 +1,63 @@
 package project.models;
 
-public class UserModel {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer id;
+
+	@Column(name = "username", unique = true, nullable = false)
+	private String username;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	public UserModel() {
+
+	}
+
+	public UserModel(Integer id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
