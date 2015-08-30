9a725c9f8191572f357ab765f5432b331314f9b4
Alejandro Durand <alejandro.durand.90@gmail.com>
2015-08-30 15:50:37 -0300

    Updated main class and build.gradle for creater json file from url

--- Project/src/main/java/project/swagger/SwaggerJson.java
+++ Project/src/main/java/project/swagger/SwaggerJson.java
@@ -1,18 +1,49 @@
 package project.swagger;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class SwaggerJson {
 	
 	public static void main(String [] args){
-		File file = new File(args[0]);
-		if (!file.exists()) {
-			if (file.mkdir()) {
-				System.out.println("Directory is created!");
-			} else {
-				System.out.println(args[0]);
-				System.out.println("Failed to create directory!");
+		//Create destination folder if not exists
+		File folder = new File(args[1]);
+		if (!folder.exists()) folder.mkdir();
+		
+		try {
+			//Open connection to get swagger json
+			URL url = new URL(args[0]);
+			URLConnection conn = url.openConnection();
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			String inputLine;
+		
+			//Save to this file name
+			File file = new File(args[0]+"/"+args[1]);
+			if (!file.exists()) {
+				file.createNewFile();
 			}
+		
+			//Use FileWriter to write file
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+		
+			while ((inputLine = br.readLine()) != null) {
+				bw.write(inputLine);
+			}
+		
+			bw.close();
+			br.close();
+		
+		} catch (IOException e) {
+			System.out.println("Error: Cannot create swagger json file");
 		}
 	}
 }
