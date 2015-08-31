0020a7ed1fa241fbdb4c9b1a2dd5eff90846e28c
Alejandro Durand <alejandro.durand.90@gmail.com>
2015-08-30 23:00:48 -0300

    Updated Database files

--- Database/real_state_agent_db.sql
+++ Database/real_state_agent_db.sql
@@ -21,36 +21,12 @@ USE `real_state_agent_db` ;
 DROP TABLE IF EXISTS `real_state_agent_db`.`user` ;
 
 CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `username` VARCHAR(45) NOT NULL COMMENT '',
-  `password` VARCHAR(45) NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC)  COMMENT '',
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC)  COMMENT '')
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `real_state_agent_db`.`real_state_agent`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `real_state_agent_db`.`real_state_agent` ;
-
-CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`real_state_agent` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `name` VARCHAR(45) NOT NULL COMMENT '',
-  `description` TEXT NULL COMMENT '',
-  `web` VARCHAR(45) NULL COMMENT '',
-  `email` VARCHAR(45) NULL COMMENT '',
-  `user_id` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`, `user_id`)  COMMENT '',
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC)  COMMENT '',
-  INDEX `fk_real_state_agent_user1_idx` (`user_id` ASC)  COMMENT '',
-  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_real_state_agent_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `real_state_agent_db`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
 ENGINE = InnoDB;
 
 
@@ -60,16 +36,28 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `real_state_agent_db`.`office` ;
 
 CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`office` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `name` VARCHAR(45) NOT NULL COMMENT '',
-  `phone` VARCHAR(45) NULL COMMENT '',
-  `real_state_agent_id` INT NOT NULL COMMENT '',
-  `real_state_agent_user_id` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`, `real_state_agent_id`, `real_state_agent_user_id`)  COMMENT '',
-  INDEX `fk_office_real_state_agent1_idx` (`real_state_agent_id` ASC, `real_state_agent_user_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_office_real_state_agent1`
-    FOREIGN KEY (`real_state_agent_id` , `real_state_agent_user_id`)
-    REFERENCES `real_state_agent_db`.`real_state_agent` (`id` , `user_id`)
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `phone` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `real_state_agent_db`.`address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `real_state_agent_db`.`address` ;
+
+CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`address` (
+  `street` VARCHAR(45) NOT NULL,
+  `suite` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `zip_code` INT NOT NULL,
+  `office_id` INT NOT NULL,
+  PRIMARY KEY (`office_id`),
+  CONSTRAINT `fk_address_office1`
+    FOREIGN KEY (`office_id`)
+    REFERENCES `real_state_agent_db`.`office` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -81,47 +69,44 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `real_state_agent_db`.`geo_location` ;
 
 CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`geo_location` (
-  `latitude` INT NOT NULL COMMENT '',
-  `longitude` INT NOT NULL COMMENT '')
+  `latitude` INT NOT NULL,
+  `longitude` INT NOT NULL,
+  `address_office_id` INT NOT NULL,
+  PRIMARY KEY (`address_office_id`),
+  CONSTRAINT `fk_geo_location_address1`
+    FOREIGN KEY (`address_office_id`)
+    REFERENCES `real_state_agent_db`.`address` (`office_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `real_state_agent_db`.`address`
+-- Table `real_state_agent_db`.`real_state_agent`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `real_state_agent_db`.`address` ;
+DROP TABLE IF EXISTS `real_state_agent_db`.`real_state_agent` ;
 
-CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`address` (
-  `street` VARCHAR(45) NOT NULL COMMENT '',
-  `suite` VARCHAR(45) NULL COMMENT '',
-  `city` VARCHAR(45) NOT NULL COMMENT '',
-  `zip_code` INT NOT NULL COMMENT '')
+CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`real_state_agent` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` TEXT NULL,
+  `web` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `real_state_agent_db`.`property_category`
+-- Table `real_state_agent_db`.`category`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `real_state_agent_db`.`property_category` ;
+DROP TABLE IF EXISTS `real_state_agent_db`.`category` ;
 
-CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`property_category` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `name` VARCHAR(45) NOT NULL COMMENT '',
-  `description` TINYTEXT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '')
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `real_state_agent_db`.`photo`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `real_state_agent_db`.`photo` ;
-
-CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`photo` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `url` VARCHAR(45) NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '',
-  UNIQUE INDEX `url_UNIQUE` (`url` ASC)  COMMENT '')
+CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` TINYTEXT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -131,34 +116,12 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `real_state_agent_db`.`property` ;
 
 CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`property` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `rooms` INT NOT NULL COMMENT '',
-  `size` INT NULL COMMENT '',
-  `description` TEXT NULL COMMENT '',
-  `state` ENUM('available', 'unavailable') NULL COMMENT '',
-  `real_state_agent_id` INT NOT NULL COMMENT '',
-  `real_state_agent_user_id` INT NOT NULL COMMENT '',
-  `property_category_id` INT NOT NULL COMMENT '',
-  `photo_id` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`, `real_state_agent_id`, `real_state_agent_user_id`, `property_category_id`, `photo_id`)  COMMENT '',
-  INDEX `fk_property_real_state_agent1_idx` (`real_state_agent_id` ASC, `real_state_agent_user_id` ASC)  COMMENT '',
-  INDEX `fk_property_property_category1_idx` (`property_category_id` ASC)  COMMENT '',
-  INDEX `fk_property_photo1_idx` (`photo_id` ASC)  COMMENT '',
-  CONSTRAINT `fk_property_real_state_agent1`
-    FOREIGN KEY (`real_state_agent_id`)
-    REFERENCES `real_state_agent_db`.`real_state_agent` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_property_property_category1`
-    FOREIGN KEY (`property_category_id`)
-    REFERENCES `real_state_agent_db`.`property_category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_property_photo1`
-    FOREIGN KEY (`photo_id`)
-    REFERENCES `real_state_agent_db`.`photo` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `rooms` INT NOT NULL,
+  `size` INT NULL,
+  `description` TEXT NULL,
+  `state` ENUM('AVAILABLE', 'UNAVAILABLE') NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -168,18 +131,129 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `real_state_agent_db`.`operation` ;
 
 CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`operation` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `type` ENUM('for_sale', 'for_rent') NOT NULL COMMENT '',
-  `price` DECIMAL NOT NULL COMMENT '',
-  `property_id` INT NOT NULL COMMENT '',
-  `property_real_state_agent_id` INT NOT NULL COMMENT '',
-  `property_real_state_agent_user_id` INT NOT NULL COMMENT '',
-  `property_property_category_id` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`, `property_id`, `property_real_state_agent_id`, `property_real_state_agent_user_id`, `property_property_category_id`)  COMMENT '',
-  INDEX `fk_operation_property1_idx` (`property_id` ASC, `property_real_state_agent_id` ASC, `property_real_state_agent_user_id` ASC, `property_property_category_id` ASC)  COMMENT '',
+  `type` ENUM('FOR_SALE','FOR_RENT') NOT NULL,
+  `price` DECIMAL NOT NULL,
+  `property_id` INT NOT NULL,
+  PRIMARY KEY (`property_id`),
+  INDEX `fk_operation_property1_idx` (`property_id` ASC),
   CONSTRAINT `fk_operation_property1`
-    FOREIGN KEY (`property_id` , `property_real_state_agent_id` , `property_real_state_agent_user_id` , `property_property_category_id`)
-    REFERENCES `real_state_agent_db`.`property` (`id` , `real_state_agent_id` , `real_state_agent_user_id` , `property_category_id`)
+    FOREIGN KEY (`property_id`)
+    REFERENCES `real_state_agent_db`.`property` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `real_state_agent_db`.`photo`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `real_state_agent_db`.`photo` ;
+
+CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`photo` (
+  `url` VARCHAR(45) NOT NULL,
+  `property_id` INT NOT NULL,
+  PRIMARY KEY (`property_id`),
+  UNIQUE INDEX `url_UNIQUE` (`url` ASC),
+  INDEX `fk_photo_property1_idx` (`property_id` ASC),
+  CONSTRAINT `fk_photo_property1`
+    FOREIGN KEY (`property_id`)
+    REFERENCES `real_state_agent_db`.`property` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `real_state_agent_db`.`user_has_real_state_agent`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `real_state_agent_db`.`user_has_real_state_agent` ;
+
+CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`user_has_real_state_agent` (
+  `user_id` INT NOT NULL,
+  `real_state_agent_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `real_state_agent_id`),
+  INDEX `fk_user_has_real_state_agent_real_state_agent1_idx` (`real_state_agent_id` ASC),
+  INDEX `fk_user_has_real_state_agent_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_real_state_agent_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `real_state_agent_db`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_real_state_agent_real_state_agent1`
+    FOREIGN KEY (`real_state_agent_id`)
+    REFERENCES `real_state_agent_db`.`real_state_agent` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `real_state_agent_db`.`office_has_real_state_agent`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `real_state_agent_db`.`office_has_real_state_agent` ;
+
+CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`office_has_real_state_agent` (
+  `office_id` INT NOT NULL,
+  `real_state_agent_id` INT NOT NULL,
+  PRIMARY KEY (`office_id`, `real_state_agent_id`),
+  INDEX `fk_office_has_real_state_agent_real_state_agent1_idx` (`real_state_agent_id` ASC),
+  INDEX `fk_office_has_real_state_agent_office1_idx` (`office_id` ASC),
+  CONSTRAINT `fk_office_has_real_state_agent_office1`
+    FOREIGN KEY (`office_id`)
+    REFERENCES `real_state_agent_db`.`office` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_office_has_real_state_agent_real_state_agent1`
+    FOREIGN KEY (`real_state_agent_id`)
+    REFERENCES `real_state_agent_db`.`real_state_agent` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `real_state_agent_db`.`real_state_agent_has_property`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `real_state_agent_db`.`real_state_agent_has_property` ;
+
+CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`real_state_agent_has_property` (
+  `real_state_agent_id` INT NOT NULL,
+  `property_id` INT NOT NULL,
+  PRIMARY KEY (`real_state_agent_id`, `property_id`),
+  INDEX `fk_real_state_agent_has_property_property1_idx` (`property_id` ASC),
+  INDEX `fk_real_state_agent_has_property_real_state_agent1_idx` (`real_state_agent_id` ASC),
+  CONSTRAINT `fk_real_state_agent_has_property_real_state_agent1`
+    FOREIGN KEY (`real_state_agent_id`)
+    REFERENCES `real_state_agent_db`.`real_state_agent` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_real_state_agent_has_property_property1`
+    FOREIGN KEY (`property_id`)
+    REFERENCES `real_state_agent_db`.`property` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `real_state_agent_db`.`category_has_property`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `real_state_agent_db`.`category_has_property` ;
+
+CREATE TABLE IF NOT EXISTS `real_state_agent_db`.`category_has_property` (
+  `category_id` INT NOT NULL,
+  `property_id` INT NOT NULL,
+  PRIMARY KEY (`category_id`, `property_id`),
+  INDEX `fk_category_has_property_property1_idx` (`property_id` ASC),
+  INDEX `fk_category_has_property_category1_idx` (`category_id` ASC),
+  CONSTRAINT `fk_category_has_property_category1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `real_state_agent_db`.`category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_category_has_property_property1`
+    FOREIGN KEY (`property_id`)
+    REFERENCES `real_state_agent_db`.`property` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
