cd43b0181a0b9c70eeec36bbf2820a6c9450bbd1
Alejandro Durand <alejandro.durand.90@gmail.com>
2015-09-21 19:07:37 -0300

    Fixed minnor error in exception EntityAlreadyExists

--- Project/src/main/java/project/exceptions/EntityAlreadyExistsException.java
+++ Project/src/main/java/project/exceptions/EntityAlreadyExistsException.java
@@ -5,6 +5,6 @@ import project.exceptions.messages.DefaultMessage;
 public class EntityAlreadyExistsException extends CustomException {
 
     public EntityAlreadyExistsException(String entityName, String fieldName, String fieldValue) {
-        super(new DefaultMessage(400, String.format("%s: %s %s already exists.", entityName, fieldName, fieldValue)));
+        super(new DefaultMessage(404, String.format("%s: %s %s already exists.", entityName, fieldName, fieldValue)));
     }
 }
